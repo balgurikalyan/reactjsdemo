860032f5f4d22e12ace224818075c5e4be3d84a8
kalyan rao <balgurikalyan711.com>
2021-04-21 22:38:44 +0530

    deployment;

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,13 @@
+import {useState} from 'react';
+
 const Create = () => {
+    const [flag,setFlag]=useState(false);
+    const link ="Blog created";
     return ( 
         <div className="create">
-            <h2>Create New Blog</h2>
-            <button>create</button>
+            <h2>Create New Blogs here</h2>
+            <button onClick={()=>setFlag(true)}>create</button>
+            {flag  && <p>{link}</p>}
         </div>
      );
 }
